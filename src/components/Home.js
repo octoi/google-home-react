7878dca15c1b28f3c0522d09e2e1aa441aa64ecd
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-07 12:59:46 +0530

    used home styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,120 +1,18 @@
-import React from 'react';
-import styled from 'styled-components'
-
-const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-`
-
-const LogoSection = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	min-height: 290px;
-	height: calc(100% - 560px);
-
-	img {
-		width: 272px;
-		height: 92px;
-		margin-top: auto;
-	}
-`
-
-const SearchSection = styled.div`
-	padding: 20px;
-	display: flex;
-	justify-content: center;
-	flex-direction: column;
-`;
-
-const Search = styled.div`
-	display: flex;
-  align-items: center;
-  padding: 0px 14px;
-  background: #fff;
-  border: 1px solid #dfe1e5;
-  box-shadow: none;
-  border-radius: 24px;
-  z-index: 3;
-  height: 44px;
-  margin: 0 auto;
-  max-width: 584px;
-`;
-
-const SearchIcon = styled.div`
-	color: #9aa0a6;
-  height: 30px;
-  width: 30px;
-  padding-right: 10px;
-  display: flex;
-  align-items: center;
-	
-  & svg {
-    fill: #9aa0a6;
-  }	
-`;
-
-const SearchInput = styled.input`
-	display: flex;
-	flex: 1;
-	height: 40px;
-	background-color: transparent;
-	border: none;
-	margin: 0;
-	padding: 0;
-	color: rgba(0, 0, 0, 0.87);
-	word-wrap: break-word;
-	outline: none;
-	height: 34px;
-	font-size: 16px;
-`;
-
-const MicIcon = styled.div`
-	display: flex;
-	cursor: pointer;
-	align-items: center;
-	border: 0;
-	background: transparent;
-	outline: none;
-	padding: 0 8px;
-	width: 40px;
-	line-height: 44px;
-	height: 44px;
-
-	svg {
-		height: 24px;
-		width: 24px;
-		vertical-align: middle;
-	}
-`;
-
-const ButtonSection = styled.div`
-	display: flex;
-	justify-content: center;
-	height: 70px;
-	padding-top: 18px;
-	top: 3px;
-	z-index: 0;
-
-	button {
-		background-color: #f8f9fa;
-    border: 1px solid #f8f9fa;
-    border-radius: 4px;
-    color: #3c4043;
-    font-family: Roboto, arial, sans-serif;
-    font-size: 14px;
-    margin: 11px 4px;
-    padding: 0 16px;
-    line-height: 27px;
-    height: 36px;
-    min-width: 54px;
-    text-align: center;
-    cursor: pointer;
-    user-select: none;
-	}
-`;
+import React, { useState } from 'react';
+import {
+	Container,
+	LogoSection,
+	SearchSection,
+	Search,
+	SearchIcon,
+	SearchInput,
+	MicIcon,
+	ButtonSection
+} from '../styles/homeStyles';
 
 const Home = () => {
+	const [query, setQuery] = useState('');
+
 	return (
 		<Container>
 			<LogoSection>
@@ -124,7 +22,9 @@ const Home = () => {
 				/>
 			</LogoSection>
 			<SearchSection>
-				<form action='/' method='GET' role='search'>
+				<form onSubmit={(e) => {
+					e.preventDefault();
+				}} method='GET' role='search'>
 					<Search>
 						<SearchIcon>
 							<svg
