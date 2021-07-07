6fe5c7ef3fb88abdf314c23d6a1177c86c556206
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-07 12:02:28 +0530

    basic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components'
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+`
 
 const Home = () => {
 	return (
-		<div>
+		<Container>
 			<h2>Hello World</h2>
-		</div>
+		</Container>
 	)
 }
 
