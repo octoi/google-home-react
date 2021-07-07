72871503d258a5c7543673ea7a9352b965429abf
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-07 12:21:24 +0530

    used home component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { CenterContainer } from '../styles/center'
+import Home from '../components/Home'
 
 const HomePage = () => {
 	return (
 		<CenterContainer>
-			<h2>Home Page</h2>
+			<Home />
 		</CenterContainer>
 	)
 }
